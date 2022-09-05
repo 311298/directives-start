5fdd580efee654e20f7fe4d720e7ac4fca72ee34
311298 <kashyapanurag109@gmail.com>
2022-09-05 12:05:16 +0530

    directive end

--- app.component.ts
+++ app.component.ts
@@ -1,11 +1,18 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"],
 })
 export class AppComponent {
   numbers = [1, 2, 3, 4, 5];
   onlyOdd = false;
+  valueForNgSwitch: number = 99;
+
+  variableForCustomStructuralDirective: boolean = false;
+  onClick() {
+    this.variableForCustomStructuralDirective =
+      !this.variableForCustomStructuralDirective;
+  }
 }
